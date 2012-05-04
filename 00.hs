d32c0dbd76f4511ea90c4c7bf4f118304dabb4cd
Jan Minárik <jan@minarik.net>
2012-05-04 09:15:32 +0200

    solved 1-10

--- 00.hs
+++ 00.hs
@@ -1,5 +1,6 @@
 import Control.Monad
 import Data.List
+import Data.Char
 
 -----------------------------------------------------------
 --
@@ -116,3 +117,119 @@ factors x
   | otherwise = p:(factors (x `div` p))
     where p = primeFactor x (squareRoot x)
 
+-----------------------------------------------------------
+--
+-- 6
+--
+-- The sum of the squares of the first ten natural numbers is,
+--   1^2 + 2^2 + ... + 10^2 = 385
+--
+-- The square of the sum of the first ten natural numbers is,
+--   (1 + 2 + ... + 10)^2 = 552 = 3025
+--
+-- Hence the difference between the sum of the squares of 
+-- the first ten natural numbers and the square of the sum 
+-- is 3025 - 385 = 2640.
+--
+-- Find the difference between the sum of the squares of
+-- the first one hundred natural numbers and the square of 
+-- the sum.
+--
+-----------------------------------------------------------
+
+eu6 = (square . sum) naturals - sum (map square naturals)
+  where square x = x * x
+        naturals = [1..100]
+
+-----------------------------------------------------------
+--
+-- 7
+--
+-- By listing the first six prime numbers: 
+--   2, 3, 5, 7, 11, and 13, we can see that the 6th prime 
+-- is 13.
+--
+-- What is the 10 001st prime number?
+--
+-----------------------------------------------------------
+
+eu7 = (filter isPrime [1..]) !! 10001
+
+
+-----------------------------------------------------------
+--
+-- 8
+--
+--Find the greatest product of five consecutive digits in the 1000-digit number.
+--
+--   73167176531330624919225119674426574742355349194934
+--   96983520312774506326239578318016984801869478851843
+--   85861560789112949495459501737958331952853208805511
+--   12540698747158523863050715693290963295227443043557
+--   66896648950445244523161731856403098711121722383113
+--   62229893423380308135336276614282806444486645238749
+--   30358907296290491560440772390713810515859307960866
+--   70172427121883998797908792274921901699720888093776
+--   65727333001053367881220235421809751254540594752243
+--   52584907711670556013604839586446706324415722155397
+--   53697817977846174064955149290862569321978468622482
+--   83972241375657056057490261407972968652414535100474
+--   82166370484403199890008895243450658541227588666881
+--   16427171479924442928230863465674813919123162824586
+--   17866458359124566529476545682848912883142607690042
+--   24219022671055626321111109370544217506941658960408
+--   07198403850962455444362981230987879927244284909188
+--   84580156166097919133875499200524063689912560717606
+--   05886116467109405077541002256983155200055935729725
+--   71636269561882670428252483600823257530420752963450
+--
+-----------------------------------------------------------
+
+eu8 :: (Integral a) => a -> Int
+eu8 x = maxProduct . map digitToInt $ show x
+
+maxProduct :: (Num a, Ord a) => [a] -> a
+maxProduct (a:b:c:d:e:[]) = a * b * c * d * e
+maxProduct xs = max (foldl1 (*) (take 5 xs)) (maxProduct . tail $ xs)
+
+-----------------------------------------------------------
+--
+-- 9
+--
+-- A Pythagorean triplet is a set of three natural numbers,
+-- a  b  c, for which,
+--     a^2 + b^2 = c^2
+--
+-- For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+--
+-- There exists exactly one Pythagorean triplet for which 
+--     a + b + c = 1000.
+--
+-- Find the product abc.
+--
+-----------------------------------------------------------
+
+eu9 = nub a
+  where 
+    a = do
+      x <- [1..500]
+      y <- [1..500]
+      let z = squareRoot (sq x + sq y)
+      guard (sq x + sq y == sq z && x + y + z == 1000)
+      return (x * y * z)
+        where sq a = a * a
+
+-----------------------------------------------------------
+--
+-- 10
+--
+-- Find the greatest product of five consecutive digits in 
+-- the 1000-digit number.
+--
+-- The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+--
+-- Find the sum of all the primes below two million.
+--
+-----------------------------------------------------------
+
+eu10 = sum $ filter isPrime [2..1999999]
