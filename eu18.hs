768bcc4ffe576227ade3314122398150d497c09b
Jan Minárik <jan@minarik.net>
2012-05-08 09:29:46 +0200

    problem 18 reworked so it solves also problem 67

--- eu18.hs
+++ eu18.hs
@@ -1,35 +1,13 @@
-import Data.List.Split
-import Data.List
+import System.Environment (getArgs)
 
-newtype Tree = Tree {runTree :: [[Int]]}
-data Pos = Pos {row :: Int, offset :: Int}
-type Pick = (Tree -> Pos -> Pos)
+main = getArgs >>= readFile . head >>= print . eu18 . parse . map words . lines
 
-(!) :: Tree -> Pos -> Int
-(!) (Tree tree) (Pos depth offset) = tree !! (depth - 1) !! (offset - 1)
+eu18 :: [[Int]] -> Int
+eu18 a = head $ foldr1 solve a
 
-depth :: Tree -> Int
-depth (Tree tree) = length tree
+solve :: [Int] -> [Int] -> [Int]
+solve [] [_] = []
+solve (x:xs) (z:y:ys) = (x + max z y) : solve xs (y:ys)
 
-childs :: Pos -> (Pos,Pos)
-childs (Pos a b) = (Pos (a+1) b, Pos (a+1) (b+1))
-
-createTree :: String -> Tree
-createTree content = Tree $ map (map readInt . (splitOn " ")) $ lines content
-  where readInt = read :: String -> Int
-
-highestChild :: Pick
-highestChild tree pos = if lVal < rVal then rPos else lPos
-  where (lPos,rPos) = childs pos
-        (lVal,rVal) = (tree ! lPos, tree ! rPos)
-
-walk :: Pick -> Tree -> Pos -> Int -> Int
-walk pick tree start steps
-  | steps == 0 = value
-  | otherwise  = value + walk pick tree (pick tree start) (steps-1)
-    where value = tree ! start
-
-eu18 = do
-  c <- readFile "data/eu18.txt"
-  let tree = createTree c
-  return $ walk highestChild tree (Pos 1 1) (depth tree - 1)
+parse :: [[String]] -> [[Int]]
+parse = map (map read)
