970b88cf91eff4bc7456a8c18b5789b6579a3854
Jan Minárik <jan@minarik.net>
2012-05-08 12:07:49 +0200

    better problem 15, still not working

--- eu15.hs
+++ eu15.hs
@@ -1,15 +1,18 @@
 
 import System.Environment (getArgs)
 
---0 1 1 1
---1 2 3 4
---1 3 6 10
---1 4 10 20
+--1  1  1  1
+--1  2  3  4
+--1  3  6 10
+--1  4 10 20
 
-paths :: (Int,Int) -> Int
-paths (1,1) = 0
-paths (x,1) = 1
-paths (1,y) = 1
-paths (x,y) = paths (x-1,y) + paths (x,y-1)
+--[1,1,1,1,1] -> [1,2,3,4,5] -> [1,3,6,10,15] ...
 
-main = getArgs >>= print . (\x -> paths (x,x)) . read . head
+paths :: Int -> [Integer]
+paths 1 = [1,1..]
+paths x = scanl1 (+) (paths (x-1))
+
+eu15 :: Int -> Integer
+eu15 x = (paths x) !! (x-1)
+
+main = getArgs >>= print . eu15 . read . head
