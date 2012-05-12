c1ae51fc7ffdb665961157537ae18765bd5e850f
Jan Minárik <jan@minarik.net>
2012-05-12 09:09:07 +0200

    problem 25 solved

---
+++ eu25.hs
@@ -0,0 +1,10 @@
+
+fib = 1 : 1 : zipWith (+) fib (tail fib)
+
+eu25 = head . dropWhile not1000Digits $ injectIndex fib
+  where not1000Digits = (< 1000) . length . show . fst
+        injectIndex a = zipWith (\x y -> (x,y)) a [1..]
+
+test = takeWhile not1000Digits $ injectIndex fib
+  where not1000Digits = (< 1000) . length . show . fst
+        injectIndex a = zipWith (\x y -> (x,y)) a [1..]
